5e04feff1f5ab8fbd5eb9f2e7e58ce9aa342557e
SuperScary <wylanshoemaker@gmail.com>
2024-06-24 08:57:52 -0700

    Tutorial 1

--- src/net/superscary/example/OurFirstProgram.java
+++ src/net/superscary/example/OurFirstProgram.java
@@ -1,5 +1,11 @@
 package net.superscary.example;
 
+/**
+ * This class is part of the Getting Started With Java series of tutorials.
+ * The tutorial can be found at the link below.
+ * https://tutorials.superscary.net/java/creating-your-first-program/
+ */
+
 public class OurFirstProgram {
     public static void main (String[] args) {
         System.out.println("Hello World!");
